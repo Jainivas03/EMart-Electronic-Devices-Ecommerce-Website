d4f962b857d96d670d7bdf6a55f6b2cb1ed2480d
Jainivas Anandhan <122606118+Jainivas03@users.noreply.github.com>
2025-03-18 11:11:07 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -5,6 +5,9 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config(); // Load environment variables from .env
 
 const port = 3000;
